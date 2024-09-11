675e5bc3169a92e604aa88c8c4b939f006778e8e
Georgy Artamonov <artamonov2018gi@gmail.com>
2024-09-11 21:18:00 +0300

    [helper] MAX(a, b) macro added

--- helper.h
+++ helper.h
@@ -4,6 +4,8 @@
 #include <stdlib.h>
 #include <stdint.h>
 
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
+
 void daemonize(const char *service);
 
 char *hexlify(const void *data, size_t len, int upper, char *out);
