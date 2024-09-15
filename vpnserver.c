b84ecc6c3a1c543a6c60abc71ea7964e3ac14ecb
Georgy Artamonov <artamonov2018gi@gmail.com>
2024-09-15 22:08:02 +0300

    [vpnserver] [vpnclient] unnecessary includes removed

--- vpnserver.c
+++ vpnserver.c
@@ -3,14 +3,9 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <signal.h>
-#include <syslog.h>
-#include <fcntl.h>
 #include <sys/time.h>
 #include <sys/types.h>
 #include <sys/select.h>
-#include <sys/socket.h>
-#include <arpa/inet.h>
 #include <errno.h>
 
 #include "network.h"
