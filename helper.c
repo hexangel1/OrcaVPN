bcb9b629ca6479dac734ccbc2b22b879e4a3c828
Georgy Artamonov <artamonov2018gi@gmail.com>
2024-09-22 23:15:18 +0300

    [helper] binarize function (hexlify inverse) added

--- helper.c
+++ helper.c
@@ -7,6 +7,7 @@
 #include <fcntl.h>
 #include <sys/stat.h>
 #include <sys/types.h>
+#include <ctype.h>
 
 #include "helper.h"
 
@@ -26,19 +27,31 @@ void daemonize(const char *service)
 	setsid();
 	if (fork() > 0)
 		exit(0);
-	openlog(service, LOG_CONS | LOG_PID, LOG_DAEMON);
+	openlog(service, LOG_PID | LOG_CONS, LOG_DAEMON);
 	syslog(LOG_INFO, "Daemon started, pid == %d", getpid());
-	atexit(&closelog);
+	atexit(closelog);
 }
 
-char *hexlify(const void *data, size_t len, int upper, char *res)
+char *hexlify(const void *bin, size_t len, int upper, char *res)
 {
 	const char *hex_digits = upper ? "0123456789ABCDEF" : "0123456789abcdef";
-	const unsigned char *bytes = data;
+	const unsigned char *bytes = bin;
 	size_t i;
 	for (i = 0; i < len; ++i) {
 		res[i * 2]     = hex_digits[bytes[i] >> 4];
 		res[i * 2 + 1] = hex_digits[bytes[i] & 0x0F];
 	}
 	return res;
+}
+
+void *binarize(const char *hex, size_t len, void *res)
+{
+	size_t i;
+	unsigned char *bytes = res;
+	for (i = 0; i < len / 2; ++i) {
+		if (!isxdigit(hex[2 * i]) || !isxdigit(hex[2 * i + 1]))
+			return NULL;
+		bytes[i] = (XDIGIT(hex[2 * i]) << 4) | (XDIGIT(hex[2 * i + 1]));
+	}
+	return bytes;
 }
--- helper.h
+++ helper.h
@@ -5,8 +5,15 @@
 
 #define MAX(a, b) ((a) > (b) ? (a) : (b))
 
+#define XDIGIT(c) (\
+	((c) >= '0' && (c) <= '9') ? ((c) - '0') : \
+	((c) >= 'a' && (c) <= 'f') ? ((c) - 'a' + 10) : \
+	((c) >= 'A' && (c) <= 'F') ? ((c) - 'A' + 10) : (-1))
+
 void daemonize(const char *service);
 
-char *hexlify(const void *data, size_t len, int upper, char *out);
+char *hexlify(const void *data, size_t len, int upper, char *res);
+
+void *binarize(const char *hex, size_t len, void *res);
 
 #endif /* HELPER_H_SENTRY */
