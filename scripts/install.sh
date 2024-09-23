2bdf8cc7efc7aad73644158640004c2d68210bf9
Georgy Artamonov <artamonov2018gi@gmail.com>
2024-09-24 01:01:36 +0300

    [scripts] [config] installation script and systemd service files added

---
+++ scripts/install.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/sh
+
+if [ $# -ne 1 ]; then
+    echo "Select configuration mode: server or client"
+    exit 1
+fi
+
+DIR_PREFIX='/usr/local'
+MODE=$1
+
+if [ $MODE != "server" ] && [ $MODE != "client" ]; then
+    echo "Invalid configuration mode"
+    exit 1
+fi
+
+set -x
+make
+install orcavpn $DIR_PREFIX/bin/
+install scripts/vpnserver-setup.sh $DIR_PREFIX/bin/
+install config/$MODE/orcavpn.conf $DIR_PREFIX/etc/ --mode=644
+install config/$MODE/orcavpn.service /etc/systemd/system/ --mode=644
+make clean
+systemctl enable orcavpn
