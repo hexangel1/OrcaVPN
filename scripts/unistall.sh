b18c4e45c485d8c6bc7c2e8e7b95c2540a900b02
Georgy Artamonov <artamonov2018gi@gmail.com>
2024-09-24 01:15:13 +0300

    [scripts] uninstall script added

---
+++ scripts/unistall.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/sh
+
+DIR_PREFIX='/usr/local'
+
+set -x
+systemctl stop orcavpn
+systemctl disable orcavpn
+rm -f $DIR_PREFIX/bin/orcavpn
+rm -f $DIR_PREFIX/bin/vpnserver-setup.sh
+rm -f $DIR_PREFIX/etc/orcavpn.conf
+rm -f /etc/systemd/system/orcavpn.service
