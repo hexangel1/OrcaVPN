4bdc748331a038b2983c5ff1b98b6d55ec862dcd
Georgy Artamonov <artamonov2018gi@gmail.com>
2024-09-29 23:01:52 +0300

    [scripts] vpnclint-setup.sh script added

---
+++ scripts/vpnclient-setup.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/sh
+
+SERVER_IP=192.168.1.10
+SERVER_TUN_IP=10.0.0.1
+DEFAULT_ROUTER=192.168.1.1
+
+if [ $# -ge 1 ]; then
+    SERVER_IP=$1
+fi
+
+if [ $# -ge 2 ]; then
+    SERVER_TUN_IP=$2
+fi
+
+if [ $# -ge 3 ]; then
+    DEFAULT_ROUTER=$3
+fi
+
+set -x
+ip route add $SERVER_IP via $DEFAULT_ROUTER
+ip route add default via $SERVER_TUN_IP
