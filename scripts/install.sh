ce538405a5db78d8ad99d2c7429bca81d796751b
Georgy Artamonov <artamonov2018gi@gmail.com>
2024-10-19 15:45:24 +0300

    [scripts] [config] orcavpn installation directory to /sbin changed
    Write binary and helper scripts to /sbin directory,
    because running of each file requires root privileges.

--- scripts/install.sh
+++ scripts/install.sh
@@ -15,8 +15,8 @@ fi
 
 set -x
 make
-install src/orcavpn $DIR_PREFIX/bin/
-install scripts/vpn$MODE-setup.sh $DIR_PREFIX/bin/
+install src/orcavpn $DIR_PREFIX/sbin/
+install scripts/vpn$MODE-setup.sh $DIR_PREFIX/sbin/
 install config/$MODE/orcavpn.conf $DIR_PREFIX/etc/ --mode=644
 install config/$MODE/orcavpn.service /etc/systemd/system/ --mode=644
 make clean
--- scripts/uninstall.sh
+++ scripts/uninstall.sh
@@ -5,8 +5,8 @@ DIR_PREFIX='/usr/local'
 set -x
 systemctl stop orcavpn
 systemctl disable orcavpn
-rm -f $DIR_PREFIX/bin/orcavpn
-rm -f $DIR_PREFIX/bin/vpnserver-setup.sh
-rm -f $DIR_PREFIX/bin/vpnclient-setup.sh
+rm -f $DIR_PREFIX/sbin/orcavpn
+rm -f $DIR_PREFIX/sbin/vpnserver-setup.sh
+rm -f $DIR_PREFIX/sbin/vpnclient-setup.sh
 rm -f $DIR_PREFIX/etc/orcavpn.conf
 rm -f /etc/systemd/system/orcavpn.service
