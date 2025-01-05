eed510add9b4684725dafff10e0e57602f108ea5
Georgy Artamonov <artamonov2018gi@gmail.com>
2025-01-05 20:36:36 +0300

    [scripts] compression and current filesize check added

--- scripts/orca-logrotate.sh
+++ scripts/orca-logrotate.sh
@@ -1,13 +1,30 @@
 #!/usr/bin/sh
 
 LOGFILE=/var/log/orcavpn.log
+FILESIZE_MB_LIMIT=10
 ROTATIONS=4
 
-set -x
-rm -f "${LOGFILE}.$ROTATIONS"
+if [ ! -f $LOGFILE ]; then
+    echo "${LOGFILE} not exists"
+    exit 1
+fi
+
+if [ $# -ne 1 ] || [ $1 != "-f" ]; then
+    LOGFILE_SIZE=$(du -m $LOGFILE | cut -f1)
+    if [ $LOGFILE_SIZE -le $FILESIZE_MB_LIMIT ]; then
+        echo "current log file size <= ${FILESIZE_MB_LIMIT}MB"
+        exit 0
+    fi
+fi
+
+rm -f "${LOGFILE}.${ROTATIONS}.gz"
+if [ -f "${LOGFILE}.1" ]; then
+    gzip "${LOGFILE}.1"
+fi
+
 for i in $(seq $(($ROTATIONS-1)) -1 1); do
-    if [ -e "${LOGFILE}.$i" ]; then
-        mv "${LOGFILE}.$i" "${LOGFILE}.$((i+1))"
+    if [ -f "${LOGFILE}.$i.gz" ]; then
+        mv "${LOGFILE}.$i.gz" "${LOGFILE}.$((i+1)).gz"
     fi
 done
 
