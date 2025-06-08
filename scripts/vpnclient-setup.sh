5ba1ffdaf32775a29d1fe82fcb3dce286595b7bd
Georgy Artamonov <artamonov2018gi@gmail.com>
2025-06-08 15:57:05 +0300

    [scripts] try to guess client default router
    The default router may differ from 192.168.1.1, try to guess
    default router address by server ip routing rule.

--- scripts/vpnclient-setup.sh
+++ scripts/vpnclient-setup.sh
@@ -2,7 +2,9 @@
 
 SERVER_IP=${SERVER_IP:-${1:-'192.168.1.1'}}
 SERVER_TUN_IP=${SERVER_TUN_IP:-${2:-'10.80.80.1'}}
-DEFAULT_ROUTER=${DEFAULT_ROUTER:-${3:-'192.168.1.1'}}
+
+GUESS_ROUTER=$(ip route get $SERVER_IP | head -n 1 | awk '/via/ { print $3 }')
+DEFAULT_ROUTER=${DEFAULT_ROUTER:-${3:-${GUESS_ROUTER:-'192.168.1.1'}}}
 
 set -x
 ip route add $SERVER_IP via $DEFAULT_ROUTER proto static
