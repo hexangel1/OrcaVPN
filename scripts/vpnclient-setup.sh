8bba3d1642c589563e8f806e8712c87613212ccc
Georgy Artamonov <artamonov2018gi@gmail.com>
2025-09-30 19:57:59 +0300

    [scripts] do not try to route server ip via tunnel

--- scripts/vpnclient-setup.sh
+++ scripts/vpnclient-setup.sh
@@ -3,7 +3,8 @@
 SERVER_IP=${SERVER_IP:-${1:-'198.51.100.49'}}
 SERVER_TUN_IP=${SERVER_TUN_IP:-${2:-'10.80.80.1'}}
 
-GUESS_ROUTER=$(ip route get $SERVER_IP | head -n 1 | awk '/via/ { print $3 }')
+GUESS_ROUTER=$(ip route get $SERVER_IP | \
+    grep -v $SERVER_TUN_IP | head -n 1 | awk '/via/ { print $3 }')
 DEFAULT_ROUTER=${DEFAULT_ROUTER:-${3:-${GUESS_ROUTER:-'192.168.1.1'}}}
 
 set -x
