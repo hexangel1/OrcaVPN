aeacbec190790df1066da66e8f7a00e9431d9c5c
Georgy Artamonov <artamonov2018gi@gmail.com>
2025-10-23 17:59:41 +0300

    [scripts] fix getting default router

--- scripts/vpnclient-setup.sh
+++ scripts/vpnclient-setup.sh
@@ -3,7 +3,7 @@
 SERVER_IP=${SERVER_IP:-${1:-'198.51.100.49'}}
 SERVER_TUN_IP=${SERVER_TUN_IP:-${2:-'10.80.80.1'}}
 
-GUESS_ROUTER=$(ip route get $SERVER_IP | \
+GUESS_ROUTER=$(ip route | grep default | \
     grep -v $SERVER_TUN_IP | head -n 1 | awk '/via/ { print $3 }')
 DEFAULT_ROUTER=${DEFAULT_ROUTER:-${3:-${GUESS_ROUTER:-'192.168.1.1'}}}
 
